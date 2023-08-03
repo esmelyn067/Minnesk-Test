bda632b0470634cf43af09fb5698a641bcec74c5
Esmelyn M. Santos <esmelyn@mail.com>
2023-08-03 00:12:10 -0400

    first version of task1

---
+++ Task1/task1.js
@@ -0,0 +1,31 @@
+//Version #1 with for and condicional
+
+function reverseNotSpecial(arr){
+    //Here we keep only all special characters and its positions
+    const specialChar= []
+    const charPosition = []
+   
+    // Iterating array elements and separating all special 
+    for (let i=0; i<arr.length; i++){
+        const index = arr[i]
+
+     if (typeof index !=='number' && typeof index !=='string' ){
+        specialChar.push(index);
+        charPosition.push(i);
+     }
+    }
+
+    //Reverse the Array and keep all special chars positions
+    arr.reverse();
+ 
+    for (let i =0; i<specialChar; i++){
+        const special = specialChar[i];
+        const position = charPosition[i];
+        array.splice(position, 0, special);
+    }
+
+  return arr
+}
+
+//Version #2 with ES6 and RegEx
+
